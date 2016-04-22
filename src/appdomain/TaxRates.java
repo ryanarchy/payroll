891d09d2160a2d84f24e24a1bd31ed6902190571
ryanarchy <ryanarchy@DESKTOP-BO5FTCT>
2016-04-22 12:21:13 -0500

    Updated Salaired employees to have the proper values

---
+++ src/appdomain/TaxRates.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appdomain;
+
+/**
+ *
+ * @author ryanarchy
+ */
+public class TaxRates
+{
+    public static final float FEDERAL_TAX_RATE = (float) 0.3;
+}
