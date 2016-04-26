bf769664aed222359c4ffc03a0f4905b8f281a55
ryanarchy <ryanarchy@sponge-ruiner.lan>
2016-04-26 11:30:14 -0500

    Changed this class to extend HourlyEmployee instead of inherting the interface.  This makes the code cleaner through not requiring replication of every method prototyped in Employee.

--- src/appdomain/SalariedEmployee.java
+++ src/appdomain/SalariedEmployee.java
@@ -5,123 +5,30 @@
  */
 package appdomain;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
+
 
 /**
  *
  * @author Alex 
  */
-public class SalariedEmployee implements Employee 
-{
-    HashMap<Short, FiscalYear> calendar;
+public class SalariedEmployee extends HourlyEmployee
+{   
+
+    public SalariedEmployee(String name, Double rate)
+    {
+        super(name, rate);
+    }
     
-    String name;  //employee name
-    Double rate;  //employee pay rate
-    // instance of calendar class for date lookups
-    Calendar cal = Calendar.getInstance();
-    
-    @Override
-    public void setName(String name)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getName()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setPayRate(double rate)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getPayRate()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public HashMap<Date, Double> getPayPeriod(short year, short period)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean hasYear(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getHours(Date date)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getTotalHours(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setHoursWorked(Date date, double hours)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getFederalTax(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getStateTax(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getMedicareTax(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getSocialSecurityTax(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getDeductions(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    // ALEX
+    /**
+     * This is the override method that will just return a flat gross salary for
+     * salaried employees. 
+     * @param year
+     * @return 
+     */
     @Override
     public double getGrossPay(short year)
     {
        return this.rate;
-    }
-    // ALEX
-    @Override
-    public double getNetPay(short year)
-    {
-        return this.rate - this.getDeductions(year);
-    }
-
-    @Override
-    public FiscalYear getFiscalYear(short year)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
